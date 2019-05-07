f31bb11b3444a20f13695e43940ccb608353839c
ClearlyHunch <edgecoding@naver.com>
2019-05-07 21:14:52 +0900

    Add Repository Test (#2)
    * Init test
    
    * Make Grade
    
    * Make GradeRepository
    
    * Add getGradeTest
    
    * Add insertTest

--- src/main/java/Domain/Grade.java
+++ src/main/java/Domain/Grade.java
@@ -1,4 +1,15 @@
 package Domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Grade {
+
+    private String grade;
+    private String subject;
+    private int credit;
+
 }
