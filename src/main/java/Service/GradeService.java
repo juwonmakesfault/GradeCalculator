82ba04777a9d30417b976b5dbfc8bb01000cd145
MyriaBreak <wotmd8534@naver.com>
2019-05-07 21:46:14 +0900

    Add ServiceTest (#4)
    * Add calculateGPA
    
    * Add FetchSubject
    
    * Add FetchGrade
    
    * Add calculateCurrentGPATest
    
    * Add FetchRealCodingGradeTest
    
    * Add FetchGradesTest

--- src/main/java/Service/GradeService.java
+++ src/main/java/Service/GradeService.java
@@ -1,4 +1,80 @@
 package Service;
 
+import Domain.Grade;
+import Repository.GradeRepository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class GradeService {
+
+   private GradeRepository gradeRepository;
+
+   public double calculateGPA(){
+       int totalCredit = 0;
+       double cGPA = 0;
+       Grade cur_grade;
+
+       Iterator<Grade> Grades = gradeRepository.getGrades().iterator();
+       while(Grades.hasNext()) {
+           cur_grade = Grades.next();
+           totalCredit += cur_grade.getCredit();
+
+           if(cur_grade.getGrade().equals("A+")){
+               cGPA += cur_grade.getCredit() * 4.5;
+           } else if(cur_grade.getGrade().equals("A0")){
+               cGPA += cur_grade.getCredit() * 4.0;
+           } else if(cur_grade.getGrade().equals("B+")){
+               cGPA += cur_grade.getCredit() * 3.5;
+           } else if(cur_grade.getGrade().equals("B0")){
+               cGPA += cur_grade.getCredit() * 3.0;
+           } else if(cur_grade.getGrade().equals("C+")){
+               cGPA += cur_grade.getCredit() * 2.5;
+           } else if(cur_grade.getGrade().equals("C0")) {
+               cGPA += cur_grade.getCredit() * 2.0;
+           } else if(cur_grade.getGrade().equals("D+")){
+               cGPA += cur_grade.getCredit() * 1.5;
+           } else if(cur_grade.getGrade().equals("D0")){
+               cGPA += cur_grade.getCredit() * 1.0;
+           } else if(cur_grade.getGrade().equals("F")){
+               cGPA += cur_grade.getCredit() * 0;
+           }
+       }
+
+       return (cGPA/totalCredit);
+   }
+  
+  
+    public String FetchSubject(String subject_name){
+        Grade cur_grade;
+
+        List<Grade> fetch_grades = new ArrayList<Grade>();
+
+        Iterator<Grade> Grades = gradeRepository.getGrades().iterator();
+        while(Grades.hasNext()) {
+            cur_grade = Grades.next();
+            if(cur_grade.getSubject().equals(subject_name))
+                return cur_grade.getGrade();
+        }
+
+        return null;
+    }
+
+    public List<Grade> FetchGrade(String find){
+        Grade cur_grade;
+
+        List<Grade> fetch_grades = new ArrayList<Grade>();
+
+        Iterator<Grade> Grades = gradeRepository.getGrades().iterator();
+        while(Grades.hasNext()) {
+            cur_grade = Grades.next();
+            if(cur_grade.getGrade().equals(find))
+                fetch_grades.add(cur_grade);
+        }
+
+        return fetch_grades;
+    }
+
+   
 }
