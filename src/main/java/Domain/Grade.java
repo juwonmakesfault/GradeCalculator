20441643d4aae48bfbee59500ca1a788e3ffd32a
clearlyhunch <edgecoding@naver.com>
2019-05-07 21:00:28 +0900

    Make Grade

--- src/main/java/Domain/Grade.java
+++ src/main/java/Domain/Grade.java
@@ -1,4 +1,14 @@
 package Domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Grade {
+
+    private String grade;
+    private String subject;
+    private int credit;
+
 }
--- src/test/java/Service/GradeServiceTest.java
+++ src/test/java/Service/GradeServiceTest.java
@@ -1,5 +1,7 @@
 package Service;
 
+import org.junit.runner.RunWith;
+
 import static org.junit.Assert.*;
 
 public class GradeServiceTest {
