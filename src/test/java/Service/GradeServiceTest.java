907a88de026c23dcd0f4f386e8a45b39cf4987be
illian01 <wnsgh578@naver.com>
2019-05-07 22:24:25 +0900

    Get sync (#8)
    * Add basic service test (#3)
    
    * Add setGradeTest
    
    * Add setCreditTest
    
    * Add ServiceTest (#4)
    
    * Add calculateGPA
    
    * Add FetchSubject
    
    * Add FetchGrade
    
    * Add calculateCurrentGPATest
    
    * Add FetchRealCodingGradeTest
    
    * Add FetchGradesTest
    
    * Add ServiceTest (#6)
    
    * Add upperCaseTest
    
    * Add gradeTest
    
    * Modify GradeTest (#7)
    
    * Add upperCaseTest
    
    * Add gradeTest
    
    * Modify GradeTest

--- src/test/java/Service/GradeServiceTest.java
+++ src/test/java/Service/GradeServiceTest.java
@@ -1,10 +1,15 @@
 package Service;
 
 import Domain.Grade;
+import Repository.GradeRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
@@ -15,6 +20,12 @@ public class GradeServiceTest {
     @Mock
     Grade grade;
 
+    @Spy
+    GradeRepository gradeRepository;
+
+    @InjectMocks
+    GradeService gradeService;
+  
     @Test
     public void setGradeTest(){
         grade.setGrade("A0");
@@ -27,5 +38,40 @@ public class GradeServiceTest {
         grade1.setCredit(3);
         assertTrue(grade1.getCredit()>=0);
     }
+
+    @Test
+    public void upperCaseTest() {
+        Grade grade = new Grade("a+", null, 0);
+        assertTrue(Character.isUpperCase(grade.getGrade().charAt(0)));
+    }
+
+    @Test
+    public void isGradeTest(){
+        Grade grade = new Grade("E0", null, 0);
+        assertThat( grade.getGrade().charAt(0) , anyOf(is('A'), is('B'), is('C'), is('D'), is('F') ));
+    }
+
+    @Test
+    public void calculateCurrentGPA(){
+        gradeRepository.insertGrade(new Grade("A+", "sub1", 3));
+        gradeRepository.insertGrade(new Grade("A+", "sub2", 3));
+        assertThat(gradeService.calculateGPA(), is(4.5));
+    }
+
+    @Test
+    public void FetchRealCodingGrade(){
+        gradeRepository.insertGrade(new Grade("A+", "실전코딩", 2));
+        assertThat(gradeService.FetchSubject("실전코딩"), is("A+"));
+    }
     
+    @Test
+    public void FetchGradesTest(){
+        gradeRepository.insertGrade(new Grade("A+", "실전코딩", 2));
+        gradeRepository.insertGrade(new Grade("B0", "IT영어", 1));
+        gradeRepository.insertGrade(new Grade("A+", "데이터통신", 3));
+        gradeRepository.insertGrade(new Grade("A+", "운영체제및실습", 3));
+        gradeRepository.insertGrade(new Grade("B+", "웹프로그래밍", 3));
+
+        assertThat(gradeService.FetchGrade("A+").size(), is(3));
+    }    
 }
