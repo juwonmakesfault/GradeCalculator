0f43936a27c16cc0b80a3a3a4fe008e289c68383
clearlyhunch <edgecoding@naver.com>
2019-05-07 21:04:33 +0900

    Add getGradeTest

--- src/test/java/Repository/GradeRepositoryTest.java
+++ src/test/java/Repository/GradeRepositoryTest.java
@@ -1,7 +1,21 @@
 package Repository;
 
+import org.junit.Test;
+import org.mockito.Spy;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.*;
 
 public class GradeRepositoryTest {
+    @Spy
+    private GradeRepository gradeRepository;
+
+    @Test
+    public void getGradeTest(){
+        assertThat(gradeRepository.getGrades(), instanceOf(List.class));
+    }
 
 }
