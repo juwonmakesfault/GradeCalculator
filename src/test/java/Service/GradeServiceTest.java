33869fcdb6a9821ae237c799623e10f98991800d
illian01 <wnsgh578@naver.com>
2019-05-07 21:34:43 +0900

    Add basic service test (#3)
    * Add setGradeTest
    
    * Add setCreditTest

--- src/test/java/Service/GradeServiceTest.java
+++ src/test/java/Service/GradeServiceTest.java
@@ -1,9 +1,31 @@
 package Service;
 
+import Domain.Grade;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class GradeServiceTest {
+    
+    @Mock
+    Grade grade;
 
+    @Test
+    public void setGradeTest(){
+        grade.setGrade("A0");
+        verify(grade,times(1)).setGrade(anyString());
+    }
+    
+    @Test
+    public void setCreditTest(){
+        Grade grade1 = new Grade(null, null, 0);
+        grade1.setCredit(3);
+        assertTrue(grade1.getCredit()>=0);
+    }
+    
 }
