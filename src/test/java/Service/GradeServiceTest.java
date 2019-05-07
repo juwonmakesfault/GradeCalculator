2419b0ff11d81764f6f0bd6ca2473299b4febfea
illian01 <wnsgh578@naver.com>
2019-05-07 21:24:29 +0900

    Add setGradeTest

--- src/test/java/Service/GradeServiceTest.java
+++ src/test/java/Service/GradeServiceTest.java
@@ -3,7 +3,18 @@ package Service;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class GradeServiceTest {
+    @Mock
+    Grade grade;
+  
 
+    @Test
+    public void setGradeTest(){
+      grade.setGrade("A0");
+      verify(grade,times(1)).setGrade(anyString());
+    }
 }
