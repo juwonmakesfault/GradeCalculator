e6444cfc2d8a84b2ea00e58b18cc99dbde5d9cd2
ClearlyHunch <edgecoding@naver.com>
2019-05-11 18:43:40 +0900

    extract method

--- src/main/java/Service/GradeService.java
+++ src/main/java/Service/GradeService.java
@@ -9,43 +9,50 @@ import java.util.List;
 
 public class GradeService {
 
-   private GradeRepository gradeRepository;
+    private GradeRepository gradeRepository;
 
-   public double calculateGPA(){
-       int totalCredit = 0;
-       double cGPA = 0;
-       Grade cur_grade;
+    public double calculateGPA(){
+        int totalCredit = 0;
+        double cGPA = 0;
+        Grade cur_grade;
 
-       Iterator<Grade> Grades = gradeRepository.getGrades().iterator();
-       while(Grades.hasNext()) {
-           cur_grade = Grades.next();
-           totalCredit += cur_grade.getCredit();
+        Iterator<Grade> Grades = gradeRepository.getGrades().iterator();
+        while(Grades.hasNext()) {
+            cur_grade = Grades.next();
+            totalCredit += cur_grade.getCredit();
 
-           if(cur_grade.getGrade().equals("A+")){
-               cGPA += cur_grade.getCredit() * 4.5;
-           } else if(cur_grade.getGrade().equals("A0")){
-               cGPA += cur_grade.getCredit() * 4.0;
-           } else if(cur_grade.getGrade().equals("B+")){
-               cGPA += cur_grade.getCredit() * 3.5;
-           } else if(cur_grade.getGrade().equals("B0")){
-               cGPA += cur_grade.getCredit() * 3.0;
-           } else if(cur_grade.getGrade().equals("C+")){
-               cGPA += cur_grade.getCredit() * 2.5;
-           } else if(cur_grade.getGrade().equals("C0")) {
-               cGPA += cur_grade.getCredit() * 2.0;
-           } else if(cur_grade.getGrade().equals("D+")){
-               cGPA += cur_grade.getCredit() * 1.5;
-           } else if(cur_grade.getGrade().equals("D0")){
-               cGPA += cur_grade.getCredit() * 1.0;
-           } else if(cur_grade.getGrade().equals("F")){
-               cGPA += cur_grade.getCredit() * 0;
-           }
-       }
+            cGPA += cur_grade.getCredit() * alpha2Number(cur_grade);
 
-       return (cGPA/totalCredit);
-   }
-  
-  
+
+        }
+
+        return (cGPA/totalCredit);
+    }
+
+    public double alpha2Number(Grade cur_grade){
+        if(cur_grade.getGrade().equals("A+")){
+            return cur_grade.getCredit() * 4.5;
+        } else if(cur_grade.getGrade().equals("A0")){
+            return cur_grade.getCredit() * 4.0;
+        } else if(cur_grade.getGrade().equals("B+")){
+            return cur_grade.getCredit() * 3.5;
+        } else if(cur_grade.getGrade().equals("B0")){
+            return cur_grade.getCredit() * 3.0;
+        } else if(cur_grade.getGrade().equals("C+")){
+            return cur_grade.getCredit() * 2.5;
+        } else if(cur_grade.getGrade().equals("C0")) {
+            return cur_grade.getCredit() * 2.0;
+        } else if(cur_grade.getGrade().equals("D+")){
+            return cur_grade.getCredit() * 1.5;
+        } else if(cur_grade.getGrade().equals("D0")){
+            return cur_grade.getCredit() * 1.0;
+        } else if(cur_grade.getGrade().equals("F")){
+            return cur_grade.getCredit() * 0;
+        }
+        return -1;
+    }
+
+
     public String FetchSubject(String subject_name){
         Grade cur_grade;
 
@@ -76,5 +83,5 @@ public class GradeService {
         return fetch_grades;
     }
 
-   
+
 }
